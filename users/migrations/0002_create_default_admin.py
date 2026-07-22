51358e2b535e53b488600ed446bc4a178ace6fae
Developer <dev@school.com>
2026-07-22 13:56:26 +0300

    Fix admin user creation in migration - use make_password

--- users/migrations/0002_create_default_admin.py
+++ users/migrations/0002_create_default_admin.py
@@ -1,19 +1,19 @@
-import os
 from django.db import migrations
+from django.contrib.auth.hashers import make_password
 
 
 def create_default_admin(apps, schema_editor):
     User = apps.get_model('users', 'User')
     if not User.objects.filter(username='admin').exists():
-        admin = User.objects.create_superuser(
+        User.objects.create(
             username='admin',
             email='admin@school.com',
-            password='admin123',
+            password=make_password('admin123'),
             role='admin',
+            is_staff=True,
+            is_superuser=True,
+            is_active=True,
         )
-        admin.is_staff = True
-        admin.is_superuser = True
-        admin.save()
 
 
 def reverse_func(apps, schema_editor):
